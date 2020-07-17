ebf0da63de97d3c425d624e53f86899c598bc534
MRD-LGC <68199729+MRD-LGC@users.noreply.github.com>
2020-07-17 15:49:42 +0800

    Update fine.sh

--- fine.sh
+++ fine.sh
@@ -22,7 +22,8 @@ echo $r"["$w"1"$r"] "$g"Spam Call"
 echo $r"["$w"2"$r"] "$g"Spam Wa"
 echo $r"["$w"3"$r"] "$g"Hack FB BruteForce"
 echo $r"["$w"4"$r"] "$g"HackCCTV"
-echo $r"["$w"5"$r"] "$g"Install Bahan"
+echo $r"["$w"5"$r"] "$g"NewbieTools"
+echo $r"["$w"6"$r"] "$g" Install Bahan"
 read -p "[?] Pilih : " pil;
 #batas gan
 if [ $pil = 1 ]
@@ -56,6 +57,13 @@ fi
 #batas gan
 if [ $pil = 5 ]
 then
+git clone https://github.com/MrT-Rex05/Newbie_Tools
+cd Newbie_Tools
+sh Bogeng.sh
+fi
+#batas gan
+if [ $pil = 6 ]
+then
 apt update && apt upgrade
 pkg install nano
 pkg install bash
@@ -66,9 +74,3 @@ pip install requests mechanize
 pip2 install requests mechanize
 pkg install php
 fi
-
-
-
-
-
-
